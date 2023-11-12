465f49a6500338f5c411842b0712107f5ff49a56
Md Altaf Hoshain Firoj <firoj.andev@gmail.com>
2023-11-13 00:47:53 +0600

    API credentilas updated

--- app/src/main/java/com/xrtech/xrmirroring/networking/ApiConstants.kt
+++ app/src/main/java/com/xrtech/xrmirroring/networking/ApiConstants.kt
@@ -22,7 +22,7 @@ object ApiConstants {
         return when (environment) {
             ApiEnvironment.DEVELOPMENT -> "IyL3CUM8bSCcjgJPkEHDPTz5CcUyKQFKL7gJi20kOnPrsNiBGJ3Y65ibzAzT6hE6"
             ApiEnvironment.STAGING -> "IyL3CUM8bSCcjgJPkEHDPTz5CcUyKQFKL7gJi20kOnPrsNiBGJ3Y65ibzAzT6hE6"
-            ApiEnvironment.PRODUCTION -> "eBE4htTu9YX83n2jYZORkoSSeEb5rdBUvbrhFPZJoDIGtGjYgShGwiUoKz75qw8B"
+            ApiEnvironment.PRODUCTION -> "0Df51dNZqcyuPaG9lTombNE1lqwac7E1BxKz5bjv1DcPdPAzBaeRIsNhGBbmlYqa"
             ApiEnvironment.STABLE -> "tQm6KFdPdUYm8FrNZunDQw8Py3BTQxzAiDtEaeX2ieAuTwLkUkxAL8beNHVBTavO"
         }
     }

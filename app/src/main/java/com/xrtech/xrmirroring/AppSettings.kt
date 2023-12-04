d81ecf6ed8ed49e908de23686ddbaf96f61cce94
Md Altaf Hoshain Firoj <firoj.andev@gmail.com>
2023-12-04 22:49:23 +0600

    some code note is added

--- app/src/main/java/com/xrtech/xrmirroring/AppSettings.kt
+++ app/src/main/java/com/xrtech/xrmirroring/AppSettings.kt
@@ -2,7 +2,17 @@ package com.xrtech.xrmirroring
 
 object AppSettings {
 
-    val isHostApp = true //Please update also at the manifets file
+
+    val isHostApp = false //Please update also at the manifets file
+
+    //during run to the vuzix device::Host device
+    //set isHostApp = true
+    //use SinkActivity to the manifest
+
+
+    //during run to the mobile device::client device
+    //set isHostApp = false
+    //use SenderActivity to the manifest
 
     val KEY_DEVICE_ID = "dev_id"
     val KEY_QR_CODE_VALIDITY = "qrvalid_till"
--- app/src/main/java/com/xrtech/xrmirroring/networking/ApiConstants.kt
+++ app/src/main/java/com/xrtech/xrmirroring/networking/ApiConstants.kt
@@ -7,7 +7,7 @@ object ApiConstants {
     //Change during build apk
     val environment: ApiEnvironment = ApiEnvironment.PRODUCTION
 
-    const val CLIENT_ID = "2"
+    const val CLIENT_ID = 2
     const val EMAIL = "api@xrsense.global"
     fun apiBaseURL(): String {
         return when (environment) {
@@ -22,7 +22,7 @@ object ApiConstants {
         return when (environment) {
             ApiEnvironment.DEVELOPMENT -> "IyL3CUM8bSCcjgJPkEHDPTz5CcUyKQFKL7gJi20kOnPrsNiBGJ3Y65ibzAzT6hE6"
             ApiEnvironment.STAGING -> "IyL3CUM8bSCcjgJPkEHDPTz5CcUyKQFKL7gJi20kOnPrsNiBGJ3Y65ibzAzT6hE6"
-            ApiEnvironment.PRODUCTION -> "0Df51dNZqcyuPaG9lTombNE1lqwac7E1BxKz5bjv1DcPdPAzBaeRIsNhGBbmlYqa"
+            ApiEnvironment.PRODUCTION -> "eBE4htTu9YX83n2jYZORkoSSeEb5rdBUvbrhFPZJoDIGtGjYgShGwiUoKz75qw8B"
             ApiEnvironment.STABLE -> "tQm6KFdPdUYm8FrNZunDQw8Py3BTQxzAiDtEaeX2ieAuTwLkUkxAL8beNHVBTavO"
         }
     }

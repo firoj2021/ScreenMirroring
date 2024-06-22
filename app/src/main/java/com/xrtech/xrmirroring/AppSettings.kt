e948b2b0807d8ee6df395508cfd033e160260408
Md Altaf Hoshain Firoj <firoj.andev@gmail.com>
2024-06-22 16:10:55 +0600

    same manifest file for both

--- app/src/main/java/com/xrtech/xrmirroring/AppSettings.kt
+++ app/src/main/java/com/xrtech/xrmirroring/AppSettings.kt
@@ -4,7 +4,7 @@ object AppSettings {
 
     //during run to the vuzix device::Host device
     //set isHostApp = true
-    var isHostApp = true
+    //var isHostApp = true
     //use SinkActivity to the manifest
 
 
@@ -12,6 +12,10 @@ object AppSettings {
     //set isHostApp = false
     //var isHostApp = false
     //use SenderActivity to the manifest
+
+
+
+    var isHostApp = true //Set true when run on vuzix as host & Set false when run on mobile device as client
 
     val KEY_DEVICE_ID = "dev_id"
     val KEY_QR_CODE_VALIDITY = "qrvalid_till"
